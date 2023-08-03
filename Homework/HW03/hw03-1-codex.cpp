7edda5d2609c8f1148cc54ba48b9a9df877e2f7e
zarlipzz <guitar30255@gmail.com>
2023-08-03 17:23:03 +0700

    ww

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -1,12 +1,27 @@
-//TODO: #8 Arrange this C coding into CODEX pattern.
 #include <stdio.h>
 #include <conio.h>
-int main(){
-    int a=10;
-int b=20,c=30,d=40; printf("This is %d + %d = %d ",a,b,a+b);
-    for(int i=0;i<d-30;i++){
-  if(i<30){
- printf("Here ");
-   if(i==8){printf("This is 8. ");}
-  }printf("ok");
-}return 0;}
+
+int main() {
+    // Declare and initialize variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+
+    // Print the addition of a and b
+    printf("This is %d + %d = %d ", a, b, a + b);
+
+    // Loop with condition
+    for (int i = 0; i < d - 30; i++) {
+        if (i < 30) {
+            printf("Here ");
+            if (i == 8) {
+                printf("This is 8. ");
+            }
+        }
+        printf("ok");
+    }
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
