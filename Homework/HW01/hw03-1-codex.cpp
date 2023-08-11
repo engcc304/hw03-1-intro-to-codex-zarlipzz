c12be793da25e74f2248119be8e86cf1b67d3c24
zarlipzz <guitar30255@gmail.com>
2023-08-11 19:35:40 +0700

    srwh

--- Homework/HW01/hw03-1-codex.cpp
+++ Homework/HW01/hw03-1-codex.cpp
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main() {
     // Declare and initialize variables
-    int a = 10;
-    int b = 20;
-    int c = 30;
-    int d = 40;
+    int a = 10 ;
+    int b = 20 ;
+    int c = 30 ;
+    int d = 40 ;
 
     // Print the addition of a and b
-    printf("This is %d + %d = %d ", a, b, a + b);
+    printf("This is %d + %d = %d ", a, b, a + b) ;
 
     // Loop with condition
     for (int i = 0; i < d - 30; i++) {
         if (i < 30) {
-            printf("Here ");
+            printf("Here ") ;
             if (i == 8) {
-                printf("This is 8. ");
+                printf("This is 8. ") ;
             }
         }
-        printf("ok");
+        printf("ok") ;
     }
 
     // Return 0 to indicate successful completion
-    return 0;
+    return 0 ;
 }
